4309a6b1314942bcb75ed018af8fc851e1ec2851
Danny <dannyrivasdev@gmail.com>
2024-02-11 03:13:18 -0500

    WIP

--- console_check.py
+++ console_check.py
@@ -2,5 +2,6 @@ import wikipedia
 
 print("       Console Check")
 print("---------------------------")
-game = input("Enter a game to check what consoles it was released on:")
-print(wikipedia.summary(game))
+# game = input("Enter a game to check what consoles it was released on:")
+game = "Gran Turismo"
+print(wikipedia.page(game).content)
