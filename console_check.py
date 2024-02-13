c2d4982fac3962eb331246780155852ceec10b09
Danny <dannyrivasdev@gmail.com>
2024-02-13 00:16:02 -0500

    WIP

--- console_check.py
+++ console_check.py
@@ -1,7 +1,21 @@
-import wikipedia
+import wikipediaapi
+
+wiki_wiki = wikipediaapi.Wikipedia('Console Check', 'en')
 
 print("       Console Check")
 print("---------------------------")
 # game_title = input("Enter a game to check what consoles it was released on:")
 game_title = "Gran Turismo (series)"
-print(wikipedia.summary(game_title, sentences = 3))
+page = wiki_wiki.page(game_title)
+print(type(page.section_by_title('Games')))
+
+
+
+
+
+
+
+
+
+
+print("Link to Wikipedia page: ", page.fullurl)
